3b61684a7c86baa0bb9ac117a1cf6ce6679fc4b6
Unknown_Energy <daniel06061@gmail.com>
2017-09-03 13:09:24 +0200

    added pom.xml, first spring boot application

---
+++ src/main/java/com/unknown_energy/baas/Application.java
@@ -0,0 +1,12 @@
+package com.unknown_energy.baas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
